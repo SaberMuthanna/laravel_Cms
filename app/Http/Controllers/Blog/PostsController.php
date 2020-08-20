b63886d955f60c70e5bc47ea1279d30c154b5f3d
Saber Muthanna <34119145+SaberMuthanna@users.noreply.github.com>
2020-08-20 20:34:05 +0300

    tag & category

--- app/Http/Controllers/Blog/PostsController.php
+++ app/Http/Controllers/Blog/PostsController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers\Blog;
 
+
 use App\Http\Controllers\Controller;
 use App\Post;
+use App\Category;
+use App\Tag;
 use App\User;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show(Post $post){
-        return view('blog.show')->with('post',$post);
+    public function show(Post $post)
+    {
+        return view('blog.show')->with('post', $post);
     }
+    public function category (Category $category){
+        return view('blog.category')->with('category', $category);
+    }
+    public function tag(Tag $tag)
+    {
+        return view('blog.tag')->with('tag', $tag);
+    }
+
 }
